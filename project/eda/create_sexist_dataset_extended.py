d9f3bf5462eeea9b3b26d43f53b3e847ba09fbbb
Akrielz <akrielz@yahoo.com>
2022-10-26 21:11:08 +0300

    updated sexist dataset

--- project/eda/create_sexist_dataset_extended.py
+++ project/eda/create_sexist_dataset_extended.py
@@ -32,27 +32,106 @@ def process_compliments():
     pass
 
 
-def process_conv_abuse():
-    # load the dataset
-    df = pd.read_csv("../data/conv_abuse/conv_abuse_emnlp_full.csv")
+def process_exist_test():
+    # load df
+    df = pd.read_csv("../data/exist/test/exist_2021_test_labeled.tsv", sep="\t")
 
-    # keep only the columns "text" and "label"
-    df = df[["text", "label"]]
+    # keep only rows with language in Englsih
+    df = df[df["language"] == "en"]
 
-    # rename the column
+    # keep only text and task1
+    df = df[["text", "task1"]]
+
+    # rename the columns
+    df = df.rename(columns={"task1": "label_sexist"})
+
+    # remap the "non-sexist" to "not sexist
+    df["label_sexist"] = df["label_sexist"].map({"non-sexist": "not sexist", "sexist": "sexist"})
+
+    # replace any @user with [USER]
+    df["text"] = df["text"].str.replace("@\w+", "[USER]")
+
+    return df
+
+
+def process_exist_train():
+    # load df
+    df = pd.read_csv("../data/exist/test/exist_2021_test_labeled.tsv", sep="\t")
+
+    # keep only rows with language in Englsih
+    df = df[df["language"] == "en"]
+
+    # keep only text and task1
+    df = df[["text", "task1"]]
+
+    # rename the columns
+    df = df.rename(columns={"task1": "label_sexist"})
+
+    # remap the "non-sexist" to "not sexist
+    df["label_sexist"] = df["label_sexist"].map({"non-sexist": "not sexist", "sexist": "sexist"})
+
+    # replace any @user with [USER]
+    df["text"] = df["text"].str.replace("@\w+", "[USER]")
+
+    return df
+
+
+def process_hate_speech():
+    pass
+
+
+def process_twitter_analysis_train():
+    # load the original dataset
+    df = pd.read_csv("../data/twitter_analysis/train.csv")
+
+    # keep only columns "tweet" and "label"
+    df = df[["tweet", "label"]]
+
+    # rename the columns
+    df = df.rename(columns={"tweet": "text", "label": "label_sexist"})
+
+    # remap the 0 to "not sexist" and 1 to "sexist"
+    df["label_sexist"] = df["label_sexist"].map({0: "not sexist", 1: "sexist"})
+
+    # replace any @user with [USER]
+    df["text"] = df["text"].str.replace("@\w+", "[USER]")
+
+    return df
+
+def process_workplace():
+    # load dataset project/data/workplace/ise_sexist_data_labeling.xlsx
+    df = pd.read_excel("../data/workplace/ise_sexist_data_labeling.xlsx")
+
+    # rename the columns "Sentences" to "text" and "Label" to "label_sexist"
+    df = df.rename(columns={"Sentences": "text", "Label": "label_sexist"})
+
+    # remap the 0 to "not sexist" and 1 to "sexist"
+    df["label_sexist"] = df["label_sexist"].map({0: "not sexist", 1: "sexist"})
+
+    return df
 
 
 
 def create_sexist_dataset_extended():
-    # load the original dataset
+    # load the datasets
     df_original = process_original()
     df_csmb = process_csmb()
+    df_exist_test = process_exist_test()
+    df_exist_train = process_exist_train()
+    df_twitter_analysis_train = process_twitter_analysis_train()
+    df_workplace = process_workplace()
 
+    # create the df list
+    df_list = [df_original, df_csmb, df_exist_test, df_exist_train, df_twitter_analysis_train, df_workplace]
 
-    df = pd.concat([df_original, df_extended], ignore_index=True)
+    # concatenate the dataframes
+    df = pd.concat(df_list)
 
-    # save the dataframe as csv
-    df.to_csv("../data/custom/train_sexism.csv", index=False)
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    # save the dataframe to csv
+    df.to_csv("../data/custom/train_sexist.csv", index=False)
 
 
 if __name__ == "__main__":
