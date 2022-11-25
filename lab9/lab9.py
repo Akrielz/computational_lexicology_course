497f426eaa124c72e1295fa814aeb99889b8e964
Akrielz <akrielz@yahoo.com>
2022-11-25 15:28:29 +0200

    lab9

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -1,42 +1,42 @@
 import nltk
 
-if __name__ == "__main__":
+def nltk_parsing():
     grammar = """
-    S -> NP VP
-    NP -> NN
-    VP -> VBD NP
-    NP -> Det NP
-    NP -> NN PP
-    NP -> DET NN
-    PP -> P NP
-    NN -> 'I' | 'rainbow' | 'lake'
-    VBD -> 'draw'
-    Det -> 'the'
-    P -> 'on'
-    
-    S -> S PP
-    
-    PP -> P NN
-    NN -> 'Anna'
-    P -> 'and'
-    NN -> 'Peter'
-    VBD -> 'draw'
-    NP -> 'us'
-    
-    S -> S NP
-    VP -> VBD
-    
-    NP -> NNS PP
-    NP -> 'We'
-    VBD -> 'ate'
-    NN -> 'octopus'
-    P -> 'and'
-    NNS -> 'shells'
-    P -> 'for'
-    NN -> 'dinner'
-    
-    PP -> P NNS
-    """
+        S -> NP VP
+        NP -> NN
+        VP -> VBD NP
+        NP -> Det NP
+        NP -> NN PP
+        NP -> DET NN
+        PP -> P NP
+        NN -> 'I' | 'rainbow' | 'lake'
+        VBD -> 'draw'
+        Det -> 'the'
+        P -> 'on'
+
+        S -> S PP
+
+        PP -> P NN
+        NN -> 'Anna'
+        P -> 'and'
+        NN -> 'Peter'
+        VBD -> 'draw'
+        NP -> 'us'
+
+        S -> S NP
+        VP -> VBD
+
+        NP -> NNS PP
+        NP -> 'We'
+        VBD -> 'ate'
+        NN -> 'octopus'
+        P -> 'and'
+        NNS -> 'shells'
+        P -> 'for'
+        NN -> 'dinner'
+
+        PP -> P NNS
+        """
 
     nltk_grammar = nltk.CFG.fromstring(grammar)
     parser = nltk.ChartParser(nltk_grammar)
@@ -52,3 +52,9 @@ if __name__ == "__main__":
     sentence = "We ate octopus and shells for dinner".split()
     for tree in parser.parse(sentence):
         print(tree)
+
+
+if __name__ == "__main__":
+    curl_command = "curl http://127.0.0.1:5000/operations > operations.json"
+    import os
+    os.system(curl_command)
