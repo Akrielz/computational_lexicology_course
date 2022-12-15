d15686f5a28b4706fa859f92bd9afc6a8e5e7325
akriel <akrielz@yahoo.com>
2022-12-15 22:59:37 +0200

    added clean dataset

--- project/eda/clean_dataset.py
+++ project/eda/clean_dataset.py
@@ -1,22 +1,38 @@
-import pandas as pd
-from neuspell import BertChecker
+from tqdm import tqdm
+from transformers import pipeline
+
+from project.pipeline.data_loader import DataLoader
 
 
-def clean_dataset(text_column: "str", csv_file: "str", output_file: "str"):
+def clean_dataset(csv_file: str, output_file: str, text_column: str = "text"):
     # read the data
-    df = pd.read_csv(csv_file)
+    data_loader = DataLoader(
+        data_path=csv_file, batch_size=16, shuffle=False, balance_data_method="none", task_column_name=None
+    )
 
     # init spell checker
-    checker = BertChecker()
-    checker.from_pretrained()
+    fix_spelling = pipeline("text2text-generation", model="oliverguhr/spelling-correction-english-base", device="cuda:0")
 
-    # clean the data
-    corrected_text = checker.correct_strings(df[text_column].values.tolist())
-    df[text_column] = corrected_text
+    corrected_texts = []
+    for batch in tqdm(data_loader):
+        text = list(batch["text"].values)
+        corrected_text = fix_spelling(text, max_length=400)
+        corrected_text = [item["generated_text"] for item in corrected_text]
+        corrected_texts.extend(corrected_text)
+
+    # copy the data_loader df
+    df = data_loader.df.copy()
+
+    # add the corrected texts
+    df[text_column] = corrected_texts
 
     # save the data
     df.to_csv(output_file, index=False)
 
 
 if __name__ == "__main__":
-    clean_dataset(text_column="text", csv_file="../data/semeval/train_all_tasks.csv", output_file="../data/custom/train_all_tasks_corrected.csv")
+    clean_dataset(
+        csv_file="../data/semeval/dev_task_a_entries.csv",
+        output_file="../data/custom/dev_task_a_entries_corrected.csv",
+        text_column="text",
+    )
